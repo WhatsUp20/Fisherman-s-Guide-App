fa323adee17b1b30f71cd5b7d6cbf6a87dd48555
Alex Nazarov <storyofspolding@gmail.com>
2021-01-18 23:06:36 +0300

    add recyclerView

--- app/src/main/java/com/example/fishermanhandbook/MainActivity.kt
+++ app/src/main/java/com/example/fishermanhandbook/MainActivity.kt
@@ -3,15 +3,29 @@ package com.example.fishermanhandbook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_content.*
+import kotlinx.android.synthetic.main.main_content.view.*
 
 class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         nav_view.setNavigationItemSelectedListener(this)
+
+        var list = ArrayList<ListItem>()
+
+        list.add(ListItem(R.drawable.som,"Som","This is a Som!"))
+        list.add(ListItem(R.drawable.nalim,"Nalim","This is a Nalim!"))
+        list.add(ListItem(R.drawable.shuca,"Shuca","This is a Shuca!"))
+        list.add(ListItem(R.drawable.caras,"Caras","This is a Caras!"))
+        rcView.hasFixedSize()
+        rcView.layoutManager = LinearLayoutManager(this)
+        rcView.adapter = MyAdapter(list, this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
